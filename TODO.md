41e6cc8a8ecbeb3beb9b0f949701ad71a471bc9c
crasr <csalfonsol@unal.edu.co>
2020-04-18 11:57:33 -0500

    Initializing project
    Initializing web project based on grafitalento web site

---
+++ TODO.md
@@ -0,0 +1,3 @@
+* Arreglar una distribucion incorrecta en las flechas de previo y siguiente en la galeria de imagenes (La flecha de next invade espacio de la de prev)
+
+* Una falla visual cuando se hace scrolling y se llega a la galeria. Las imagenes mientras se acomodan, generan efectos no deseados para el usuario.
---
+++ js/custom.js
@@ -0,0 +1,79 @@
+
+
+/* NOTES
+
+
+*/
+
+/* jQuery Pre loader
+--> window.load fires later (or at the same time in the worst/failing cases) when images and such are loaded, so if you're using image dimensions for example, you often want to use this instead
+-----------------------------------------------*/
+$(window).load(function(){
+
+    $('.preloader').fadeOut(1500); // set duration in brackets
+});
+
+/*
+--> document.ready is a jQuery event, it runs when the DOM is ready, e.g. all elements are there to be found/used, but not necessarily all content.
+------------------------------------------------*/
+$(document).ready(function() {
+
+  /* Hide mobile menu after clicking on a link
+    -----------------------------------------------*/
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+
+  /* home slider section
+  ----------------------------------------------- 
+  $(function(){
+    jQuery(document).ready(function() {
+    $('#home').backstretch([
+       "images/home.png",
+       "images/home.png",
+        ],  {duration: 2000, fade: 1250});
+    });
+  }) */
+
+  /* Owl Carousel
+    -----------------------------------------------*/
+  $(document).ready(function() {
+    $("#owl-testimonial").owlCarousel({
+      autoPlay: 5000,
+      items : 2,
+      itemsDesktop : [1199,1],
+      itemsDesktopSmall : [979,2],
+    });
+  });
+
+
+  /* Parallax section
+    -----------------------------------------------*/
+  function initParallax() {
+    $('#home').parallax("100%", 0.1);
+    $('#service').parallax("100%", 0.3);
+    $('#about').parallax("100%", 0.2);
+    $('#team').parallax("100%", 0.3);
+    $('#portfolio').parallax("100%", 0.1);
+    $('#blog').parallax("100%", 0.2);
+    $('#faq').parallax("100%", 0.1);
+    $('#testimonial').parallax("100%", 0.3);
+    $('#contact').parallax("100%", 0.1);
+
+  }
+  initParallax();
+
+
+  /* Nivo lightbox
+    -----------------------------------------------*/
+  $('#portfolio .col-md-4 a').nivoLightbox({
+        effect: 'fadeScale',
+    });
+
+
+  /* wow
+  -------------------------------*/
+  new WOW({ mobile: false }).init();
+
+  });
