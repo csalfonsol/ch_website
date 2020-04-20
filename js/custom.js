e4515cd591b317967597f84e3b671ccd31fdab50
crasr <csalfonsol@unal.edu.co>
2020-04-20 01:31:39 -0500

    Some issues fixed during deploy

--- js/custom.js
+++ js/custom.js
@@ -10,7 +10,7 @@
 -----------------------------------------------*/
 $(window).load(function(){
 
-    $('.preloader').fadeOut(1500); // set duration in brackets
+    $('.preloader').fadeOut(5500); // set duration in brackets
 });
 
 /*
